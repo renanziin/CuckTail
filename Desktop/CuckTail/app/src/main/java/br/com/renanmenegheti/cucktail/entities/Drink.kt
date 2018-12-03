f790b722d28f3e77c15b50b4240de81fab376454
renanziin <renan.menegheti@hotmail.com>
2018-12-03 17:25:50 -0300

    Criado o projeto e a classe Drink - esta já com os atributos importantes do JSON da API. Dependencias e permissões adicionadas.

---
+++ Desktop/CuckTail/app/src/main/java/br/com/renanmenegheti/cucktail/entities/Drink.kt
@@ -0,0 +1,8 @@
+package br.com.renanmenegheti.cucktail.entities
+
+data class Drink(val idDrink: String, val strDrink: String, val strInstructions: String,
+                 val strIngredient1: String, val strIngredient2: String, val strIngredient3: String,
+                 val strIngredient4: String, val strIngredient5: String, val strIngredient6: String,
+                 val strMeasure1: String, val strMeasure2: String, val strMeasure3: String,
+                 val strMeasure4: String, val strMeasure5: String, val strMeasure6: String,
+                 val strDrinkThumb: String)
---
+++ Desktop/CuckTail/app/src/main/java/br/com/renanmenegheti/cucktail/scenarios_main/ListaDrinks.kt
@@ -0,0 +1,13 @@
+package br.com.renanmenegheti.cucktail.scenarios_main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.renanmenegheti.cucktail.R
+
+class ListaDrinks : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_drinks)
+    }
+}
